b94218042908dd124ce9ba9188db384f39f5ff44
Noroze <106557207+Noroze@users.noreply.github.com>
2022-08-28 14:59:29 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,7 +10,8 @@ export const Footer = () => (
       <a href="https://www.privacypolicygenerator.info/live.php?token=vJFQtmBoQamU1ZN1aZumnMI9aBc6AzwY">&nbsp;Privacy Policy |</a> 
       <a href="https://www.termsandconditionsgenerator.com/live.php?token=GK9QmwJCh97QKmZe42NK1lYCn8iRDscz">&nbsp;Terms & Conditions |</a>
       <Link href='/About'>&nbsp;About us |</Link> 
-      <Link href='/Contact'>&nbsp;Contact us</Link> 
+      <Link href='/Contact'>&nbsp;Contact us</Link>
+  <Link href='/Terms'>&nbsp;Terms & Conditions |</Link>
     </div> 
     
     © 2022 Hoff-lu.com
